851c2d099f24e64ee893d1607dd02ab592ca6dad
M. Rafly Saputra <966raflisaputra@gmail.com>
2025-10-18 02:16:47 +0700

    lagi

--- src/components/ui/chatInput.tsx
+++ src/components/ui/chatInput.tsx
@@ -7,7 +7,7 @@ import { Textarea } from "./textarea"
 const ChatInput = memo(({ input, setInput, send, wait }: any) => (
     <footer className="rounded-md flex justify-between items-center gap-2 pt-2 w-full">
         <div className="w-full relative">
-            <Textarea value={input} className="bg-slate-200 max-h-[4rem] pe-16 overflow-y-auto resize-none focus-visible:ring-slate-950/30 border border-slate-400" placeholder="Masukan text..." onChange={(e) => setInput(e.target.value)} />
+            <Textarea value={input} className="bg-slate-200 max-h-[4rem] pe-16 overflow-y-auto resize-none focus-visible:ring-slate-950/70 border border-slate-400" placeholder="Masukan text..." onChange={(e) => setInput(e.target.value)} />
             <Button disabled={input.length == 0 || wait} className="cursor-pointer w-12 h-12 absolute right-3 bottom-2" onClick={() => send(input)}>{wait ? <Loader className="animate-spin" /> : <Send />}</Button>
         </div>
     </footer>
